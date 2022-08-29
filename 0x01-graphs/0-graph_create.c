f17019f948cd9f153d569e92f676395d67d78164
samilo-del <camilovalenciapadilla@gmail.com>
2022-08-29 18:15:51 -0500

    add point 0

---
+++ 0x01-graphs/0-graph_create.c
@@ -0,0 +1,15 @@
+#include "graphs.h"
+
+graph_t *graph_create(void)
+{
+	graph_t *new_graph;
+
+	new_graph = malloc(sizeof(graph_t));
+	if (!new_graph)
+		return (NULL);
+
+	new_graph->nb_vertices = 0;
+	new_graph->vertices = NULL;
+
+	return (new_graph);
+}
